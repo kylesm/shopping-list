ac67d220169babe086ced0ae73fd259bae6ac386
Kyle Smith <kylesm@gmail.com>
2013-03-23 18:36:55 -0400

    Only generate test data when in dev

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,22 +1,29 @@
+import grails.util.Environment
+
 import shopping.list.ShoppingList
 import shopping.list.ListItem
 
 class BootStrap {
 
     def init = { servletContext ->
-    	ShoppingList.withTransaction { status ->
-			def list = new ShoppingList([
-				description: 'My shopping list', completed: false, items: []
-			])
-			
-			list.items.addAll([
-				new ListItem([name: 'Green pepper', quantity: 3, obtained: false, order: 1]),
-				new ListItem([name: 'Butter', quantity: 1, obtained: false, order: 2]),
-				new ListItem([name: 'Frozen pizza', quantity: 2, obtained: false, order: 3])
-			])
-		
-			list.save()
-    	}
+		switch (Environment.current) {
+		case Environment.DEVELOPMENT:
+			ShoppingList.withTransaction { status ->
+				def list = new ShoppingList([
+					description: 'My shopping list', completed: false, items: []
+				])
+
+				list.items.addAll([
+					new ListItem([name: 'Green pepper', quantity: 3, obtained: false, order: 1]),
+					new ListItem([name: 'Butter', quantity: 1, obtained: false, order: 2]),
+					new ListItem([name: 'Frozen pizza', quantity: 2, obtained: false, order: 3])
+				])
+
+				list.save()
+			}
+
+			break
+		}
     }
     def destroy = {
     }
